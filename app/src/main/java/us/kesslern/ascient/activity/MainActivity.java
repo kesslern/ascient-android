027e794a59e12c8fb0b9e338b138dcf77316b884
kesslern <nathant93@gmail.com>
2017-08-21 20:56:31 -0400

    Remove unused import

--- app/src/main/java/us/kesslern/ascient/activity/MainActivity.java
+++ app/src/main/java/us/kesslern/ascient/activity/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
