0aaa95b28e47bcd73813005bf17ec38acf2378a8
kesslern <nathant93@gmail.com>
2017-08-23 23:00:44 -0400

    Make code use more kotlin features

--- app/src/main/java/us/kesslern/ascient/activity/MainActivity.kt
+++ app/src/main/java/us/kesslern/ascient/activity/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.IntentFilter
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
@@ -23,12 +25,12 @@ import us.kesslern.ascient.service.PermissionHandlerService
 import us.kesslern.ascient.util.RestTemplateFactory
 import us.kesslern.ascient.util.bindView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity() {
 
     private var totalReceived = 0
     private var preferences: SharedPreferences? = null
     private val intentFilter = IntentFilter("android.provider.Telephony.SMS_RECEIVED")
-    private var smsReceiver: SMSBroadcastReceiver? = null
+    private val smsReceiver = SMSBroadcastReceiver()
     private var androidId: String? = null
 
     private val totalSentTextView: TextView by bindView(R.id.totalSent)
@@ -37,17 +39,22 @@ class MainActivity : AppCompatActivity() {
     private val registerButton: Button by bindView(R.id.bottomButton)
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG, "Started main activity.")
         super.onCreate(savedInstanceState)
+        preferences = getPreferences(Context.MODE_PRIVATE)
+        androidId = AndroidIdService.getAndroidId(this)
         setContentView(R.layout.activity_main)
-
-        bindDependencies()
-        initializeSwitch()
-        totalSentTextView.setText(R.string.no_messages_received)
         PermissionHandlerService.requestPermissions(this)
+
+        smsReceiver.activity = this
+        totalSentTextView.setText(R.string.no_messages_received)
         uuidTextView.text = CLIENT_TOKEN + "\n" + androidId
 
         registerButton.setOnClickListener { register() }
+
+        syncSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            updateBroadcastReceiver(isChecked)
+        }
+        syncSwitch.isChecked = preferences?.getBoolean(ENABLED_SWITCH_STATE, false) ?: false
     }
 
     fun update() {
@@ -56,8 +63,8 @@ class MainActivity : AppCompatActivity() {
                 getString(R.string.total_received),
                 totalReceived.toString())
     }
-
-    fun updateBroadcastReceiver(enabled: Boolean) {
+    
+    private fun updateBroadcastReceiver(enabled: Boolean) {
         if (enabled) {
             registerReceiver(smsReceiver, intentFilter)
         } else {
@@ -87,20 +94,6 @@ class MainActivity : AppCompatActivity() {
                     RestTemplateFactory.build().getForObject<PhoneClient>(completeUri, PhoneClient::class.java)
                 }
         )
-    }
-
-    private fun initializeSwitch() {
-        syncSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
-            updateBroadcastReceiver(isChecked)
-        }
-        syncSwitch.isChecked = preferences!!.getBoolean(ENABLED_SWITCH_STATE, false)
-    }
-
-    private fun bindDependencies() {
-        smsReceiver = SMSBroadcastReceiver()
-        smsReceiver!!.activity = this
-        preferences = getPreferences(Context.MODE_PRIVATE)
-        androidId = AndroidIdService.getAndroidId(this)
     }
 
     companion object {
