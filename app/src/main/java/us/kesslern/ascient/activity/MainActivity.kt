2619b75c803fa4767a1fede3a8947e73812c6e85
kesslern <nathant93@gmail.com>
2018-05-02 19:56:09 -0400

    Update all dependencies, remove kotterknife

--- app/src/main/java/us/kesslern/ascient/activity/MainActivity.kt
+++ app/src/main/java/us/kesslern/ascient/activity/MainActivity.kt
@@ -6,16 +6,13 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.widget.Button
-import android.widget.Switch
-import android.widget.TextView
-import butterknife.bindView
 import com.github.kittinunf.fuel.core.FuelManager
+import kotlinx.android.synthetic.main.activity_main.*
 import us.kesslern.ascient.R
-import us.kesslern.ascient.receiver.SMSBroadcastReceiver
 import us.kesslern.ascient.authentication.RegistrationService
-import us.kesslern.ascient.tag
 import us.kesslern.ascient.permission.PermissionHandlerService
+import us.kesslern.ascient.receiver.SMSBroadcastReceiver
+import us.kesslern.ascient.tag
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,11 +21,6 @@ class MainActivity : AppCompatActivity() {
     private val preferences: SharedPreferences by lazy { getPreferences(Context.MODE_PRIVATE) }
     private val intentFilter = IntentFilter("android.provider.Telephony.SMS_RECEIVED")
     private val smsReceiver = SMSBroadcastReceiver(this)
-
-    private val totalSentTextView: TextView by bindView(R.id.totalSent)
-    private val uuidTextView: TextView by bindView(R.id.uuid)
-    private val syncSwitch: Switch by bindView(R.id.switchSync)
-    private val registerButton: Button by bindView(R.id.bottomButton)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,25 +35,25 @@ class MainActivity : AppCompatActivity() {
         }
         val applicationId = preferences.getString("applicationId", "")
 
-        totalSentTextView.setText(R.string.no_messages_received)
-        uuidTextView.text = applicationId
+        totalSent.setText(R.string.no_messages_received)
+        uuid.text = applicationId
 
-        registerButton.setOnClickListener {
+        bottomButton.setOnClickListener {
             RegistrationService.register(applicationId,
                     { d -> Log.d(TAG, "Register data: " + d) },
                     { err -> Log.e(TAG, "Register error: " + err) })
 
         }
 
-        syncSwitch.setOnCheckedChangeListener { _, isChecked ->
+        switchSync.setOnCheckedChangeListener { _, isChecked ->
             updateBroadcastReceiver(isChecked)
         }
-        syncSwitch.isChecked = preferences.getBoolean(ENABLED_SWITCH_STATE, false)
+        switchSync.isChecked = preferences.getBoolean(ENABLED_SWITCH_STATE, false)
     }
 
     fun update() {
         totalReceived++
-        totalSentTextView.text = String.format(
+        totalSent.text = String.format(
                 getString(R.string.total_received),
                 totalReceived.toString())
     }
